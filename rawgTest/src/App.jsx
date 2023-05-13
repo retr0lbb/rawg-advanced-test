15b94f07489309aec696264deed183d3bde7913b
Henrique Barbosa Sampaio <85702153+retr0lbb@users.noreply.github.com>
2023-05-13 01:53:36 -0300

    updated the search mecanism

--- rawgTest/src/App.jsx
+++ rawgTest/src/App.jsx
@@ -1,26 +1,14 @@
 import Game from "./components/Game"
 import './App.css'
-import { useState } from 'react';
+
 
 
 function App() {
-  const [page, setPage] = useState(1);
-  const mudarPagina = (novaPagina) => {
-    if(novaPagina >= 1){
-      setPage(novaPagina);
-    }
-    
-  };
 
   return (
     <>
-      <Game page={page}/>
+      <Game />
 
-      <div className="test">
-      <button onClick={() => mudarPagina(page+1)}>Passa pra proxima</button>
-      <p>pagina atual, {page}</p>
-      <button onClick={() => mudarPagina(page-1)}>Voltar</button>
-      </div>
     </>
   )
 }
--- rawgTest/src/components/Game/index.jsx
+++ rawgTest/src/components/Game/index.jsx
@@ -5,28 +5,39 @@ import './index.css'
 
 export default (props) =>{
 
-  function searchManager(page, pageCount){
-    if(!page>=pageCount){
+  const chageGamePages=(gamePage)=>{
+    if(gamePage > 1 && gamePage <= pageCount){
+      setPage(gamePage)
+    }
 
+  }
+  const handleSearch = (e) =>{
+    setSearch(e.target.value)
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+  const checkPageBundle =(pages) =>{
+    if(pages >0){
+      setPageCount(Math.floor(pages /20))
     }
   }
 
-
-
     const [jogos, setJogos] = useState([]);
-    const pesquisa = ''
+    const [search, setSearch] = useState('');
     const [pageCount ,setPageCount] = useState([]);
+    const [page, setPage] = useState(1);
 
-
+    
     const API_KEY = "9fe9df9f628245ccbef2da5be661f072"
     const URL_ENDPOINT = "https://api.rawg.io/api/games"
 
     const queryParams = {
         key: API_KEY,
-        page: props.page,
+        page: page,
         page_size:'-rating',
         platforms: '18,1,7',
-        search: `${pesquisa}`
+        search: `${search}`
     };
 
     const url = `${URL_ENDPOINT}?${new URLSearchParams(queryParams)}`
@@ -36,21 +47,35 @@ export default (props) =>{
           .then(response => response.json())
           .then(data => {
             setJogos(data.results);
-            setPageCount(Math.floor(data.count /20))
+            checkPageBundle(data.count)
           })
           .catch(e => {
             console.log(e);
           });
-      }, [url]);
+      }, [url], [page]);
 
       console.log(pageCount);
 
       return(
+        <>
         <div className='jogos'>
         {jogos.map((jogo, index) => (
             <Games key={index} src={jogo.background_image} title={jogo.name}/>
       ))}
         </div>
+
+        <div className='pages'>
+          <button onClick={()=>chageGamePages(page +1)}>Proxima</button>
+          <p>{page}</p>
+          <button onClick={()=>chageGamePages(page -1)}>Voltar</button>
+        </div>
+
+        <div>
+          <form action="GET" onSubmit={handleSubmit}>
+          <input type="text"  onChange={handleSearch}/> game name</form>
+          <button>Pesquisar</button>
+        </div>
+        </>
       )
     
 
